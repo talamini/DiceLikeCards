99b27f972cda7f2229e40289b768ea8d948c0cc3
talamini <talamini714@gmail.com>
2017-06-15 17:33:38 -0400

    Initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject dice-like-cards "0.1.0-SNAPSHOT"
+  :description "Like drawing dicerolls from a deck."
+  :dependencies [
+                 [org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.170"]
+                 [org.omcljs/om "1.0.0-alpha34"]
+                 [figwheel-sidecar "0.5.0-SNAPSHOT"  :scope "test"]])
---
+++ src/dice_like_cards/core.cljs
@@ -0,0 +1,59 @@
+(ns dice-like-cards.core
+  (:require [goog.dom :as gdom]
+            [om.next :as om :refer-macros [defui]]
+            [om.dom :as dom]))
+
+(def app-state (atom {:count 0}))
+
+(defn read [{:keys [state] :as env} key params]
+      (let [st @state]
+           (if-let [[_ value] (find st key)]
+                   {:value value}
+                   {:value :not-found})))
+
+(defn mutate [{:keys [state] :as env} key params]
+      (if (= 'increment key)
+        {:value {:keys [:count]}
+         :action #(swap! state update-in [:count] inc)}
+        {:value :not-found}))
+
+(defui Counter
+       static om/IQuery
+       (query [this]
+              [:count])
+       Object
+       (render [this]
+               (let [{:keys [count]} (om/props this)]
+                    (dom/div nil
+                             (dom/span nil (str "Count: " count))
+                             (dom/button
+                               #js {:onClick
+                                    (fn [e] (om/transact! this '[(increment)]))}
+                               "Click me!")))))
+
+(def reconciler
+  (om/reconciler
+    {:state app-state
+     :parser (om/parser {:read read :mutate mutate})}))
+
+(om/add-root! reconciler
+              Counter (gdom/getElement "app"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
